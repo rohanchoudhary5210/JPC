133e18f93f347b08ec2c2232ef2b1c0cde24b5ea
rohanchoudhary5210 <rohan5210work@gmail.com>
2022-10-15 14:13:36 +0530

    n

--- python/demo.py
+++ python/demo.py
@@ -1,3 +1,6 @@
 print("Hello this is my first change")
 print("""This is my second change
 I LOVE CODING""")
+print('Hi my name is "Rohan"')
+print("Hi my name is 'Rohan'")
+print("""Hi my name is "Rohan" """)
