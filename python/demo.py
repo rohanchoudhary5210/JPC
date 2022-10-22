da8b168744588a4600732e52fc30e021f9fb1b58
rohanchoudhary5210 <rohan5210work@gmail.com>
2022-10-22 16:55:36 +0530

    n

--- python/demo.py
+++ python/demo.py
@@ -3,4 +3,6 @@ print("""This is my second change
 I LOVE CODING""")
 print('Hi my name is "Rohan"')
 print("Hi my name is 'Rohan'")
-print("""Hi my name is "Rohan" """)
+print("""Hi my name 
+is "Rohan" """)
+  #say Hello to the students
